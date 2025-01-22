94ec19c8fdc20e845d7bca2d9a5f9426b816f86e
phillipskr <krphillips@usgs.gov>
2025-01-22 16:24:58 -0500

    update sv$folder query

--- R/estimateLake.R
+++ R/estimateLake.R
@@ -177,11 +177,13 @@ estimateLake <-
     # 1. new function/argument implementation
     # Prompts user for EBA from unique transducers
     ##################################################################
-    EV_filename_parts <- strsplit(sv$EV_filename, "[/]")
+    EV_filename_parts <- strsplit(gsub("\\\\", "/", sapply(EV_filename), "/")strsplit(sv$EV_filename, "[/]")
+    
     sv$EVfolder <- sapply(
-      strsplit(gsub("\\\\", "/", sapply(EV_filename_parts, tail, n = 1L)), "/"), 
-      function(x) if(length(x) >= 2) x[length(x) - 1] else NA
+      strsplit(gsub("\\\\", "/", sapply(EV_filename_parts, tail, n = 1L)), "/"),
+      function(x) if(length(x) >= 2) x[length(x) - 2] else NA
     )
+    
     
     sv$dat.source <-paste0(sv$EVfolder, " - ", sv$Frequency, " kHz")
     ev.source.freq <- unique(sv[c("EVfolder", "Frequency")])
